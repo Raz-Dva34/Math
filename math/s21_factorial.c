4b8ce938840bd01629d3d7037a6a8310be2653d3
Raz-Dva34 <110310591+Raz-Dva34@users.noreply.github.com>
2023-01-14 19:02:49 +0300

    Delete math directory

--- math/s21_factorial.c
+++
@@ -1,9 +0,0 @@
-//
-// Created by Killed Hizdahr on 03.01.2023.
-//
-#include "s21_math.h"
-
-int s21_factorial(int x) {
-
-  return x;
-}
--- math/s21_log.c
+++
@@ -1,9 +0,0 @@
-//
-// Created by Killed Hizdahr on 03.01.2023.
-//
-#include "s21_math.h"
-
-long double s21_log (double x) {
-
-  return x;
-}
